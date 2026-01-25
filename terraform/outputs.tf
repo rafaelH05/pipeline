34eb66760dec325f8f0375df1333b4f0e0158db4
rafaelH05 <rafa2005lara@gmail.com>
2026-01-25 18:27:48 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,12 @@
 output "rds_endpoint"{
   value = aws_db_instance.db_principal.address
 }
+output "vm1_ip" {
+  value = aws_instance.vm1.public_ip
+}
+output "vm2_private_ip" {
+  value = aws_instance.vm2.private_ip
+}
+output "vm3_private_ip" {
+  value = aws_instance.vm_monitor.private_ip
+}
